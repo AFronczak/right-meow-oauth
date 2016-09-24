64542fb9c2b5f39b3ceb6017aa6aae9c13e06e9a
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-09-24 16:14:19 -0400

    redundant manual routes removed, UsersController corrected from UserController

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
 
-  get 'homepage/index'
   root 'homepage#index'
 
   get    '/auth/:provider'          => 'omniauth#auth',    as: :auth
@@ -12,6 +11,5 @@ Rails.application.routes.draw do
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   get '/logout' => 'session#destroy'
-  get '/edit' => 'user#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
