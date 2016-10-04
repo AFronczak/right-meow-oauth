5e57c94d606f15a48c9ea93309556079c4651c9e
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-10-04 15:22:14 -0400

    forms corrected and clouds removed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navbar_footer_class
+    logged_in? ? "navbar-bottom" : "navbar-fixed-bottom"
+  end
+
+  def body_class
+    logged_in? ? "logged-in" : "logged-out"
+  end
 end
